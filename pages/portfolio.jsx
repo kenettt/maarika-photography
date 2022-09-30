8dc1055760c2fb66dc328817ea2db61147942929
Kenet Kauksi <kauksifd@gmail.com>
2022-09-30 14:38:55 +0300

    add next link

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -8,31 +8,38 @@ import "yet-another-react-lightbox/styles.css";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Link from 'next/link'
+
 export default function Portfolio() {
 
     const [index, setIndex] = useState(-1);
-    const [open, setOpen] = useState(false);
-    console.log(index)
-
 
     return (
         <div className="p-2 lg:p-0 lg:max-w-[96%] mx-auto">
           <div className="flex items-center sticky top-0 z-50  p-3 bg-white ">
-            <a href="/" className="text-lg tracking-widest font-light font-europa">Maarika Kauksi Photography</a>
+            <Link href="/" >
+             <a className="text-lg tracking-widest font-light font-europa">Maarika Kauksi Photography</a>
+            </Link>
             <div className="tracking-widest space-x-3 md:space-x-10 text-lg font-medium font-europa ml-auto ">
-                <a href="/">Avaleht</a>
-                <a href="/">About</a>
-                <a href="/">Contact</a>
-                <a href="/">Blog</a>
-                <a href="/blog">Price</a>
+              <Link href="/">
+                <a>Avalehlt</a>
+              </Link>
+              <Link href="/">
+                <a>About</a>
+              </Link>
+              <Link href="/">
+                <a>Contact</a>
+              </Link>
+              <Link href="/">
+                <a>Blog</a>
+              </Link>
+              <Link href="/">
+                <a>Price</a>
+              </Link>
             </div>
           </div>
           <div className="">
             <GridGallery images={photos} setIndex={setIndex} />
-            <button type="button" onClick={() => setOpen(true)}>
-              Open Lightbox
-            </button>
-
             <Lightbox
               open={index >= 0}
               slides={photos}
