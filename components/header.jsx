8abbeefc5069594d7b899a460f8b2cb68a4d370d
Kenet Kauksi <kauksifd@gmail.com>
2022-10-12 22:26:02 +0300

    mobile header

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,195 @@
 import Link from 'next/link'
+import { Dialog, Transition } from '@headlessui/react'
+import { MenuAlt2Icon } from '@heroicons/react/outline'
+
+import { Fragment, useState } from 'react'
+
+import { useRouter } from 'next/router'
+
+
+const navigation = [
+    { 
+      name: 'Avaleht',
+      href: '/',
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+
+      )
+    },
+    { 
+      name: 'Portfolio',
+      href: '/portfolio',
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Minust', 
+      href: '/minust',
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+
+      )
+    },
+    { 
+      name: 'Blog',
+      href: '/blog', 
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Hinnakiri',
+      href: '/hinnakiri', 
+      current: false,
+      ready:false,
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+        </svg>
+      )
+    }
+  ]
+                          
+  const socials = [
+    { name: 'Facebook', 
+      link: 'https://www.facebook.com/profile.php?id=100083092881002',
+      icon: (props) => (
+        <svg xmlns="http://www.w3.org/2000/svg" className='fill-gray-700'width="20" height="20" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 10h-2v2h2v6h3v-6h1.82l.18-2h-2v-.833c0-.478.096-.667.558-.667h1.442v-2.5h-2.404c-1.798 0-2.596.792-2.596 2.308v1.692z"/></svg>
+      )
+    },
+    { name: 'Instagram', 
+    link: 'https://www.instagram.com/maarikakauksiphotography/?hl=en',
+    icon: (props) => (
+      <svg xmlns="http://www.w3.org/2000/svg" className='fill-gray-700' width="20" height="20" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
+    )
+    }
+  ]
 
 export default function header({className=""}) {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const router = useRouter();
+
+  const setColor = function(href,ready,current) {
+    if(router.asPath === href || current) return "text-pink"
+    if(ready && router.asPath !== href) { return "text-[#5C5972] hover:text-[#FAF8FF]"} else { return "text-[#3F3C51] lg:hover:text-[#FAF8FF]" }
+  }
+
     return (
-      <div className={`${className} flex items-center sticky top-0 z-50 md:px-6 p-6 -mb-[77px] bg-gradient-to-b from-[#7b6c75b5]`}>
+      <>
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setSidebarOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-80" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <div className="relative flex-1 flex flex-col max-w-[265px] w-full bg-[#ffd9d98f]">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="absolute top-0 right-0 -mr-12 pt-2">
+                <button
+                  type="button"
+                  className="ml-1 flex items-center text-white justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  onClick={() => setSidebarOpen(false)}
+                > 
+                  <span className="sr-only">Close sidebar</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+
+                </button>
+              </div>
+            </Transition.Child>
+            <nav className="px-4 h-full shadow-lg mt-4">
+              <div className='h-full flex flex-col max-h-[600px] justify-between'>
+                <Link href="/" >
+                  <a className="text-sm text-center text-white tracking-widest font-medium font-europa max-w-[200px]">Maarika Kauksi Photography</a>
+                </Link>
+                <div>
+                  {navigation.map(link =>
+                    <div className={`flex items-center`} key={link.name}>
+                      <Link href={link.href}>
+                        <a className='flex items-center py-3 w-full text-gray-800'>
+                          <div className='ml-[5px]'>
+                            <link.icon className='h-auto w-[20px]' router={router} current={`${link.current}`} />
+                          </div>
+                          <div className={`ml-3 font-medium whitespace-nowrap text-sm`}>{link.name}</div>
+                        </a>
+                      </Link>
+                    </div>      
+                  ) 
+                  }
+                </div>
+
+               
+                <div>
+                  <div className={`text-text flex items-center`}>
+                    <div className='uppercase whitespace-nowrap text-xs font-medium text-black'>Lingid </div>
+                  </div>
+                  {socials.map(link => 
+                    <div className={`flex items-center`} key={link.name}>
+                      <Link href={link.link}>
+                        <a className='flex items-center py-3 w-full' target="_blank" rel="noopener">
+                          <div className='ml-[5px]'>
+                            <link.icon className='h-[20px] w-[20px]' />
+                          </div>
+                          <div className={`ml-3 font-medium whitespace-nowrap text-sm text-gray-800`}>{link.name}</div>
+                        </a>
+                      </Link>
+                    </div> 
+                  )}
+                </div>
+              </div>  
+            </nav>
+          </div>
+        </Transition.Child>
+      </Dialog>
+    </Transition.Root>
+   {!sidebarOpen && <button
+        type="button"
+        className="px-4 text-white focus:outline-none fixed top-2 left-2 mr-4 mt-4 z-10 lg:hidden"
+        onClick={() => setSidebarOpen(true)}
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
+    </button>}
+    <div className={`${className} hidden lg:flex items-center sticky top-0 z-50 md:px-6 p-6 -mb-[77px] bg-gradient-to-b from-[#7b6c75b5]`}>
         <Link href="/" >
           <a className="text-sm text-center md:text-start md:text-lg text-white tracking-widest font-light font-europa hidden md:block">Maarika Kauksi Photography</a>
         </Link>
@@ -24,5 +211,8 @@ export default function header({className=""}) {
           </Link>
         </div>
       </div>
+    </>
+    
+
     );
 }
