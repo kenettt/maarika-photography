b4112a41ab26c916b2feddda0c7f6fbcb24c02fc
Kenet Kauksi <kauksifd@gmail.com>
2024-11-20 08:23:32 +0200

    add  images and filter them on portfolio page

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -13,9 +13,39 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 import path from "path";
 import sizeOf from "image-size";
 import fs from "fs";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+const kategooria = [
+  {
+    label: "Kõik",
+    value: "all",
+  },
+  {
+    label: "Pere",
+    value: "pere",
+  },
+  {
+    label: "Pulm",
+    value: "pulm",
+  },
+  {
+    label: "Portree",
+    value: "portree",
+  },
+  {
+    label: "Üritus",
+    value: "uritus",
+  },
+  /*   {
+    label: "Kinnisvara",
+    value: "kinnisvara",
+  }, */
+];
 
 export default function Portfolio({ images }) {
   const [index, setIndex] = useState(-1);
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState("all");
   const animationDuration = 500;
   const maxZoomPixelRatio = 5;
   const zoomInMultiplier = 1;
@@ -26,6 +56,11 @@ export default function Portfolio({ images }) {
   const doubleClickDelay = 300;
   const doubleClickMaxStops = 5;
   const keyboardMoveDistance = 40;
+
+  useEffect(() => {
+    const { kategooria } = router.query || {};
+    setSelectedCategory(kategooria || "all");
+  }, [router.query]);
 
   return (
     <div>
@@ -39,6 +74,21 @@ export default function Portfolio({ images }) {
       </Head>
       <Header />
       <div className="lg:pl-[90px] min-h-screen h-screen w-full flex flex-col">
+        <div className="flex my-10 mx-4 sm:text-lg justify-center font-semibold max-w-[700px] mx-auto flex-wrap dark:text-black z-20">
+          {kategooria.map((category, index) => (
+            <button
+              key={index}
+              onClick={() => router.push(`?kategooria=${category.value}`)}
+              className={`${
+                selectedCategory === category.value
+                  ? "border-b-2 border-pink-300"
+                  : ""
+              } uppercase m-2`}
+            >
+              {category.label}
+            </button>
+          ))}
+        </div>
         <PhotoAlbum
           renderPhoto={NextJsImage}
           sizes={{
@@ -80,11 +130,15 @@ export default function Portfolio({ images }) {
   );
 }
 
-export async function getStaticProps() {
-  const portfolioDirectory = path.join(
-    process.cwd(),
-    "public/images/portfolio"
-  );
+export async function getServerSideProps({ query }) {
+  const { kategooria = "all" } = query || {};
+
+  const baseDirectory = path.join(process.cwd(), "public/images/portfolio");
+
+  const portfolioDirectory =
+    kategooria === "all"
+      ? `${baseDirectory}/all`
+      : path.join(baseDirectory, kategooria);
 
   let images = [];
 
@@ -93,22 +147,24 @@ export async function getStaticProps() {
     const filenames = fs.readdirSync(portfolioDirectory);
 
     images = filenames
-      .filter((file) => /\.(jpg|jpeg|png|gif|webp)$/.test(file)) // Filter only image files
+      .filter((file) => /\.(jpg|jpeg|png|gif|webp)$/.test(file))
       .map((filename) => {
         const filePath = path.join(portfolioDirectory, filename);
-        const dimensions = sizeOf(filePath); // Get dimensions using image-size
+        const dimensions = sizeOf(filePath);
 
         return {
-          src: `/images/portfolio/${filename}`,
-          width: dimensions.width, // Original width
-          height: dimensions.height, // Original height
+          src: `/images/portfolio/${kategooria}/${filename}`,
+          width: dimensions.width,
+          height: dimensions.height,
         };
-      });
+      })
+      .reverse();
   }
 
   return {
     props: {
       images,
+      selectedCategory: kategooria,
     },
   };
 }
